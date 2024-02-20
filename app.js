53eadb29def37c6b9f07ad121361e79436e5c8d2
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2024-02-20 20:59:35 +0530

    v1

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const path = require('path');
+const rootDir = require('./util/path');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
+
+app.listen(3000);
