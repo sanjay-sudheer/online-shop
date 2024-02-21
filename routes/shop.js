ce9cea38e4f135449cb5e5af58de5b762823ae0e
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2024-02-22 01:00:22 +0530

    working on dynamic file serving

--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const rootDir = require('../util/path');
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/',(req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log('shop.js',adminData.products);
 });
 
 module.exports = router;
