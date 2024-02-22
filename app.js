630d9cb60510b651e38ff37c74c4b5660162bfbd
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2024-02-23 01:59:59 +0530

    ejs integration

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const rootDir = require('./util/path');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,7 +14,7 @@ const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
