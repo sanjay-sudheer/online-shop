2f8d33bca47170be580c371e274329a701b47c16
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2024-02-23 01:04:26 +0530

    working on dynamic file serving

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 const path = require('path');
-const rootDir = require('./util/path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(rootDir, 'public')));
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin',adminData.routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,22 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
 const rootDir = require('../util/path');
-const router = express.Router();
 const adminData = require('./admin');
 
-router.get('/',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    console.log('shop.js',adminData.products);
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
